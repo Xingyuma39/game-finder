e0edf3f7184946938a460933f1a16517b52e7a38
Xingyuma39 <xingyuma39@gmail.com>
2024-03-07 17:08:23 -0500

    upgraded card styling

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -6,19 +6,20 @@ import { Link } from "react-router-dom"
 export default function Card({ game, updateDetails }) {
 
     return (
-        <figure className="relative mb-4 break-inside-avoid-column border-4 border-gray-900 bg-[#b6c2d4] shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out text-green-200">
+        <>
             <Link to={"/details/" + game?.name} onClick={() => updateDetails(game)}>
-                <div className="card-overlay absolute cursor-pointer w-full flex items-center justify-center bg-black z-10">
-                    <img src={eyeIcon} className="w-10"></img>
+                <div className="mb-4 relative break-inside-avoid-column max-w-4xl bg-slate-700 w-full rounded-lg shadow-xl border border-gray-500">
+                    <div className="p-4 border-b border-gray-500 hover:bg-gray-700 flex">
+                        <h1 className="text-green-400 font-bold text-lg ml-3 p-0 truncate">
+                            {game?.name}
+                        </h1>
+                        <h1 className="text-lg ml-auto text-green-100">{game?.rating + '⭐'}</h1>
+                    </div>
+                    <div className="p-4 border-b border-gray-500 hover:bg-gray-700 flex shadow-lg hover:shadow-2x1 transition-shadow duration-300 ease-in-out">
+                        <img src={game?.background_image} className="card-image w-full min-h-[300px] max-h-[300px] object-cover cursor-pointer border border-gray-500"/>
+                    </div>
                 </div>
             </Link>
-
-            <img src={game?.background_image} className="card-image min-h-[300px] max-h-[300px] min-w-full object-cover cursor-pointer"/>
-            
-            <figcaption className="py-2 pr-4 pl-2 flex justify-between items-center bg-slate-600">
-                <h1 className="text-lg truncate">{game?.name}</h1>
-                <h1 className="text-lg truncate">{game?.rating + '⭐'}</h1>
-            </figcaption>
-        </figure>
+        </>
     )
 }
